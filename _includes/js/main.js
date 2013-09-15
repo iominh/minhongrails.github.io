b51b9ae9f1553a5db54393c6944b4a8543c9c513
mtnguyen <minh.9.nguyen@gmail.com>
2013-09-15 15:10:34 -0400

    cleanup of commented out code

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -13,42 +13,6 @@ $(document).ready(function() {
     $('#projectsLink').click(function() {
         window.location = "/projects.html";
     });
-//    var searching = false;
-//    function flipSearch() {
-//        if (searching === true) {
-//            searching = false;
-//            $("#searchDiv").hide();
-//        } else {
-//            searching = true;
-//            $("#searchDiv").show();
-//            $("#searchBox").focus();
-//        }
-//    }
-
-//    function performSearch() {
-//        var query = $('#searchBox').val();
-//        if (query !== "") {
-//            window.location.replace("/search.html?tags=" + query);
-//        }
-//    }
-
-//    $("#searchLink").click(function() {
-//        flipSearch();
-//    });
-
-//    $("#cancelSearchLink").click(function() {
-//        flipSearch();
-//    });
-
-//    $("#searchButton").click(function() {
-//        performSearch();
-//    });
-
-//    $('#searchBox').keypress(function(e) {
-//        if (e.which == 13) {
-//            performSearch();
-//        }
-//    });
 
     // production
     $.get('/autocomplete.json', function(data) {
